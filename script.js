4be8c722e4032b04888779deddc555477f41f75a
hamza9 <hamza.elkhoukhi@gmail.com>
2025-10-12 20:16:34 +0200

    update menu mobile

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ function toggleProject(id) {
 }
 
 
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+
+
+
